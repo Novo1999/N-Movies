f46d089e2c6e3c1bab9d679af32c0f451821f9a0
Novodip Mondal <neptune.navo@gmail.com>
2023-08-17 01:27:52 +0600

    layout

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
 import Paginate from "./Paginate";
+
+const dummyArr = Array.from({ length: 10 }, (_, i) => {
+  return i;
+});
 
 function Movies() {
   return (
     <section className="border-2 col-span-3 row-span-2 relative">
-      Movies
+      <h1 className="text-white font-bold text-xl text-center">Movies</h1>
+      <div className="grid grid-cols-5 p-10 ml-5">
+        {dummyArr.map((i) => {
+          return (
+            <img
+              className="w-48 h-64 mb-12 rounded"
+              key={i}
+              src="https://dummyimage.com/300"
+              alt=""
+            />
+          );
+        })}
+      </div>
       <Paginate />
     </section>
   );
--- src/Components/Series.jsx
+++ src/Components/Series.jsx
@@ -1,5 +1,24 @@
+const dummyArr = Array.from({ length: 7 }, (_, i) => {
+  return i;
+});
+
 function Series() {
-  return <section className="border-2 col-span-2 row-span-1">Series</section>;
+  return (
+    <section className="border-2 col-span-2 row-span-1">
+      <div className="grid grid-cols-4 h-48">
+        {dummyArr.map((i) => {
+          return (
+            <img
+              className="w-36 h-44 rounded m-auto mt-5 mb-6"
+              key={i}
+              src="https://dummyimage.com/300"
+              alt=""
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Series;
